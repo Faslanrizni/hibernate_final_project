ada7ee25e18fbd4b567d287c064258469e2569c9
Faslan Rizni <Faslan2002Rizni@gmail.com>
2023-09-19 09:23:16 +0530

    student Form Ui Completed

---
+++ src/MainFormController.java
@@ -0,0 +1,17 @@
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class MainFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddressees;
+    public Button btnSaveStudent;
+    public TableView tbleStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colDelete;
+    public TableColumn colUpdate;
+}
